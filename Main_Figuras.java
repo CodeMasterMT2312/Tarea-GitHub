4cf0a48ee7789a595bb1c5e7456b6b8bb325d5d7
IronEva2312 <agusmaty23@gmail.com>
2024-05-29 19:57:01 -0500

    Main Figuras Ejemplo1

---
+++ Main_Figuras.java
@@ -0,0 +1,13 @@
+package Tarea_Github;
+
+public class Main_Figuras {
+    public static void main(String[] args) {
+        Rectangulo figura1 = new Rectangulo("Green",8.5,16.2);
+        figura1.calcularArea();
+        figura1.calcularPerimetro();
+        System.out.println("\n");
+        Triangulo figura2 = new Triangulo("Red",6.00,9.00,10.00);
+        figura2.calcularArea();
+        figura2.calcularPerimetro();
+    }
+}
